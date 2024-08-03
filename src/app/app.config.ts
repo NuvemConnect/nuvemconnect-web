cd1bcb3ae2283b8df4cd2ff2c274851d4e3f46df
laurourbano <74082012+laurourbano@users.noreply.github.com>
2024-08-03 16:04:03 -0300

    chore: Remove trailing commas in code files

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -8,6 +8,6 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideClientHydration(),
-  ],
+    provideClientHydration()
+  ]
 };
